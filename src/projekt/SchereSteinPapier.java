78f1459211a9ec8fe3683f270774eca9e56bd5c7
anita <anita@anitas-laptop.fritz.box>
2024-06-06 12:31:09 +0200

    test push

--- src/projekt/SchereSteinPapier.java
+++ src/projekt/SchereSteinPapier.java
@@ -8,6 +8,8 @@ public class SchereSteinPapier {
 	String Stein = "Stein";
 	String Papier = "Papier";
 	
+	boolean spielenWir = false;
+	
 	Scanner sc = new Scanner(System.in);
 	String[] StrArray = new String[3];
 	
@@ -16,6 +18,24 @@ public class SchereSteinPapier {
 		StrArray[0] = "Schere"; 
 		StrArray[1] = "Stein";  
 		StrArray[2] = "Papier"; 
+		
+		System.out.println("Hey willst du mit mir eine Runde Schere, Stein Papier spielen?");
+		System.out.println("Okey, let's go:");
+		
+		spielen();
+		
+	}
+	
+	
+	public void spielen() {
+		
+		spielenWir = true;
+		
+		while(spielenWir == true) {
+			
+			
+			
+		}
 		
 	}
 	
