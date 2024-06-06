6278d6bfa61ed526bc6f580a5f411217c00378cb
anita <anita@anitas-laptop.fritz.box>
2024-06-06 12:28:29 +0200

    The Starting of the Schere Stein papier Spiel

---
+++ src/projekt/Main.java
@@ -0,0 +1,11 @@
+package projekt;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		SchereSteinPapier ssp = new SchereSteinPapier();
+		System.out.println(ssp.StrArray);
+	}
+
+}
---
+++ src/projekt/SchereSteinPapier.java
@@ -0,0 +1,24 @@
+package projekt;
+
+import java.util.Scanner;
+
+public class SchereSteinPapier {
+	
+	String Schere = "Schere";
+	String Stein = "Stein";
+	String Papier = "Papier";
+	
+	Scanner sc = new Scanner(System.in);
+	String[] StrArray = new String[3];
+	
+	public SchereSteinPapier() {
+		
+		StrArray[0] = "Schere"; 
+		StrArray[1] = "Stein";  
+		StrArray[2] = "Papier"; 
+		
+	}
+	
+	
+
+}
