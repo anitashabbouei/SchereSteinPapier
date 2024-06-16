87d79092cda7f75c5a2ae7909d21b2d1aa6996d4
anita <anita@anitas-laptop.fritz.box>
2024-06-16 08:29:42 +0200

    16.06.24 weitere Implementierung

--- src/projekt/SchereSteinPapier.java
+++ src/projekt/SchereSteinPapier.java
@@ -1,56 +1,63 @@
 package projekt;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class SchereSteinPapier {
+
+//	String Schere = "Schere";
+//	String Stein = "Stein";
+//	String Papier = "Papier";
 	
-	String Schere = "Schere";
-	String Stein = "Stein";
-	String Papier = "Papier";
-	
+	int comSpielStand; 
+	int spieSpielStand; 
+
 	boolean spielenWir = false;
-	
+
 	Scanner sc = new Scanner(System.in);
 	String[] StrArray = new String[3];
-	
+
 	public SchereSteinPapier() {
+
+		StrArray[0] = "Schere";
+		StrArray[1] = "Stein";
+		StrArray[2] = "Papier";
 		
-		StrArray[0] = "Schere"; 
-		StrArray[1] = "Stein";  
-		StrArray[2] = "Papier"; 
-		
+		comSpielStand = 0; 
+		spieSpielStand = 0; 
+
 		System.out.println("Hey willst du mit mir eine Runde Schere, Stein Papier spielen?");
 		System.out.println("Okey, let's go:");
-		
+
 		spielen();
-		
+
 	}
-	
-	
+
 	public void spielen() {
-		
+
 		spielenWir = true;
-		
-		while(spielenWir == true) {
-			
-			if(!sc.hasNext("Stop")) {
-				
-			} 
-			
-			else if(sc.hasNext("Stop")) {
-				spielenWir = false; 
+
+		while (spielenWir == true) {
+
+			if (!sc.hasNext("Stop")) {
+
+				Random ran = new Random();
+
+				int nxt = ran.nextInt(2);
+
+				System.out.println(nxt);
+
+			}
+
+			else if (sc.hasNext("Stop")) {
+				spielenWir = false;
 				sc.close();
 				System.out.println("Supper! Es war sehr schön mit dir zu spielen!");
-				
-				
+
 			}
-			
-			
-			
+
 		}
-		
+
 	}
-	
-	
 
 }
